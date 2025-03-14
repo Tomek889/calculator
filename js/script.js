8aee0460168b3a7bb582016c9af56bb4411cba2e
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-03-14 20:46:28 +0100

    Create functionality with the display

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+const DIGITS = '0123456789';
+const OPERATORS = '+-*/';
+
+
+const buttons = document.querySelector('.buttons');
+const display = document.querySelector('#display');
+
+
 function add(a, b) {
     return a + b;
 }
@@ -35,4 +43,26 @@ function operate(a, b, operator) {
 let firstOperand;
 let secondOperand;
 let operator;
+let displayContent = '';
 
+
+buttons.addEventListener('click', (event) => {
+    const buttonText = event.target.textContent;
+
+    if (DIGITS.includes(buttonText)) {
+        displayContent += buttonText;
+        display.textContent = displayContent;
+    } else if (OPERATORS.includes(event.target.textContent)) {
+        if (!operator) {
+            operator = buttonText;
+            firstOperand = displayContent;
+            displayContent = '';
+            display.textContent = displayContent;
+        }
+    } else if (event.target.textContent === '=') {
+        secondOperand = displayContent;
+        display.textContent = operate(parseFloat(firstOperand), parseFloat(secondOperand), operator);
+        displayContent = display.textContent;
+        operator = '';
+    }
+});
